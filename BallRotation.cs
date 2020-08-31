bb78143c36ba3f37e5260bab39094d341d973217
Ahmet <53380248+Ahmet-Dev@users.noreply.github.com>
2020-09-01 00:22:30 +0300

    Update BallRotation.cs

--- BallRotation.cs
+++ BallRotation.cs
@@ -44,38 +44,24 @@ public class BallRotation : MonoBehaviour {
             // can't rotate ball
             return;
         }
-
-        // distance the ball traveled between the last Update and this one
-        // we subtract the vectors in this order to use it as a direction vector later
         Vector3 currentToLast = lastPosition - transform.position;
 
         // segment length that the ball rolled along its surface
         float segment = currentToLast.magnitude;
 
         if (segment == 0) {
-            // no distance travelled, nothing to do
             return;
         }
 
-        // to build a Quaternion to hold the desired rotation, 
-        // we need a Vector3 "axis" and an angle "theta"
-        // first, the axis:
-
-        // define the down direction vector for the ball
-        // the ball rolls in the x and y directions,
-        // and positive z points to the ground
-        // Important: 	this is DIFFERENT from Vector3.down because Vector3 assumes 
-        //				a 3D world space where negative y is down
+        // to build a Quaternion to hold the desired rotation
         Vector3 ballDown = new Vector3(0, 0, 1);
 
         // use Cross Product to find the axis of rotation 
-        // https://www.mathsisfun.com/algebra/vectors-cross-product.html
         Vector3 axis = Vector3.Cross(ballDown, currentToLast);
 
         // Cross Product will fail if both vectors are parallel or perpendicular
         if (axis == Vector3.zero) {
             // this should never happen because currentToLast.z is always 0
-            // but who knows where this code will be copy-pasted to...
             return;
         }
 
@@ -92,8 +78,6 @@ public class BallRotation : MonoBehaviour {
         Quaternion q = Quaternion.AngleAxis(thetaDegrees * 3.9f, axis);
 
         // apply rotation to the transform
-        // q must come first in multiplication order!!!
-        // Quaternion multiplication is Non-Commutative!
         sphere3D.transform.rotation = q * sphere3D.transform.rotation;
     }
 
